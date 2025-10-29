0e4171f5ceacc67a9ed3803fec536e33aca07b29
HarshJ2000 <harsh976324@gmail.com>
2025-10-29 16:48:28 +0530

    fixed the  way hashmap will be printed

--- day1_Text_Analyzer_CLI/src/main.rs
+++ day1_Text_Analyzer_CLI/src/main.rs
@@ -3,13 +3,17 @@
 use std::collections::HashMap;
 
 fn main() {
+    //------------------------------ METHOD 1 -----------------------------
+    //---------------- This method takes the ownership which shouldn't be done -----------------
     let mut word_counts: HashMap<String, u32> = HashMap::new();
-    let str = String::from("This that this why one too when too");
+    let text = String::from("This that this why one too when too");
 
-    for word in str.split_whitespace() {
+    for word in text.split_whitespace() {
         let word_string = word.to_lowercase().to_string();
         *word_counts.entry(word_string).or_insert(0) += 1;
     }
 
-    println!("{:?}", word_counts);
+    for (word, count) in &word_counts {
+        println!("{}:{}", word, count);
+    }
 }
