adc772f43dfddce12a6da001ef4f3195f30f7dbc
HarshJ2000 <harsh976324@gmail.com>
2025-10-29 15:27:34 +0530

    Initialized Project

---
+++ day1_Text_Analyzer_CLI/src/main.rs
@@ -0,0 +1,7 @@
+/* DAY 1 -> BUILDING A TEXT ANALYZER CLI THAT COUNTS WORD FREQUENCY */
+
+fn main() {
+    let str = String::from(
+        "This is the sentence which will be provided as input for counting word frequency!",
+    );
+}
---
+++ day1_Text_Analyzer_CLI/target/debug/deps/day1_Text_Analyzer_CLI-1fd0aa0deeda99f0.d
@@ -0,0 +1,5 @@
+/home/harsh/Projects/rust_projects/solana-hardcore-30-days/day1_Text_Analyzer_CLI/target/debug/deps/libday1_Text_Analyzer_CLI-1fd0aa0deeda99f0.rmeta: src/main.rs
+
+/home/harsh/Projects/rust_projects/solana-hardcore-30-days/day1_Text_Analyzer_CLI/target/debug/deps/day1_Text_Analyzer_CLI-1fd0aa0deeda99f0.d: src/main.rs
+
+src/main.rs:
