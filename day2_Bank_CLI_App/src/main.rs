7f2d643d43d311784728a12a0aa530ad776d10c2
HarshJ2000 <harsh976324@gmail.com>
2025-10-30 23:10:24 +0530

    function for depositing money

--- day2_Bank_CLI_App/src/main.rs
+++ day2_Bank_CLI_App/src/main.rs
@@ -3,14 +3,32 @@ pub struct Account {
     balance: f64,
 }
 
-pub enum TransactionType {
-    DepositMoney(u64),
-    WithdrawMoney(u64),
-}
+// pub enum TransactionType {
+//     DepositMoney(u64),
+//     WithdrawMoney(u64),
+// }
 
 pub enum TransactionErr {
     InsufficientBalance,
     InvalidAmt,
+}
+
+impl Account {
+    pub fn new(account_number: u32, balance: f64) -> Self {
+        Account {
+            account_number,
+            balance: 0.0,
+        }
+    }
+
+    pub fn depositMoney(&mut self, amount: f64) {
+        if amount > 0.0 {
+            self.balance += amount;
+            println!("Deposited: {}, New balance: {}", amount, self.balance);
+        } else {
+            println!("Invalid Amount!!!!!");
+        }
+    }
 }
 
 fn main() {
@@ -20,5 +38,6 @@ fn main() {
     };
 
     println!("{}", user1.account_number);
+    println!("{}", user1.balance);
     println!("{}", user1.balance);
 }
