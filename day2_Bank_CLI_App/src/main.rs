6adf55b07014d9d53cf3786b224fb973a292a119
HarshJ2000 <harsh976324@gmail.com>
2025-10-30 21:04:36 +0530

    Initialized rust project

---
+++ day2_Bank_CLI_App/src/main.rs
@@ -0,0 +1,8 @@
+struct Account {
+    name: String,
+    balance: u64,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ day2_Bank_CLI_App/target/debug/deps/day2_Bank_CLI_App-380952d0094ce374.d
@@ -0,0 +1,5 @@
+/home/harsh/Projects/rust_projects/solana-hardcore-30-days-RUST/day2_Bank_CLI_App/target/debug/deps/libday2_Bank_CLI_App-380952d0094ce374.rmeta: src/main.rs
+
+/home/harsh/Projects/rust_projects/solana-hardcore-30-days-RUST/day2_Bank_CLI_App/target/debug/deps/day2_Bank_CLI_App-380952d0094ce374.d: src/main.rs
+
+src/main.rs:
