2dbc364dac4439655a630da5d845e865209d4f9b
HarshJ2000 <harsh976324@gmail.com>
2025-10-31 20:59:18 +0530

    Renamed day1 and day2, also initialized day3 project

--- day2_Bank_CLI_App/target/debug/deps/day2_Bank_CLI_App-83222239e4e0e696.d
+++
@@ -1,5 +0,0 @@
-/home/harsh/Projects/rust_projects/solana-hardcore-30-days-RUST/day2_Bank_CLI_App/target/debug/deps/libday2_Bank_CLI_App-83222239e4e0e696.rmeta: src/main.rs
-
-/home/harsh/Projects/rust_projects/solana-hardcore-30-days-RUST/day2_Bank_CLI_App/target/debug/deps/day2_Bank_CLI_App-83222239e4e0e696.d: src/main.rs
-
-src/main.rs:
