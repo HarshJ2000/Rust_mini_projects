b184f8c04f11310977e02e818ddf5195f5ad0967
HarshJ2000 <harsh976324@gmail.com>
2025-11-01 01:06:09 +0530

    created counter_contract using accounts iteration, pattern matching for instruction types, serailizing & deserializing structs, enums and counter_data

--- day3_counter_program/src/lib.rs
+++ day3_counter_program/src/lib.rs
@@ -1,20 +1,47 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{AccountInfo, next_account_info},
+    entrypoint,
     entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
 };
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(BorshSerialize, BorshDeserialize)] // For serializing and deserializing 
 struct Counter {
     count: u32,
 }
 
 #[derive(BorshSerialize, BorshDeserialize)]
 enum InstructionType {
+    // Options created using enum for instructions
     Increment(u32),
     Decrement(u32),
 }
 
-pub fn counter_contract() -> ProgramResult {
+entrypoint!(counter_contract);
+pub fn counter_contract(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instruction_type {
+        InstructionType::Decrement(value) => {
+            msg!("Decrementing......");
+            counter_data.count -= value;
+        }
+        InstructionType::Increment(value) => {
+            msg!("Incrementing......");
+            counter_data.count += value;
+        }
+    }
+
+    counter_data.serialize(&mut *acc.data.borrow_mut())?;
+    msg!("Counter updated to: {}", counter_data.count);
+
     Ok(())
 }
