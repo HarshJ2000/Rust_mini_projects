31b613dfdb5eb49f72d15a345f2e722c546ebc21
HarshJ2000 <harsh976324@gmail.com>
2025-11-01 21:13:48 +0530

    Testing how CLI arguments are received

--- day4_optional_cli_tool/src/main.rs
+++ day4_optional_cli_tool/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        let arg1 = &args[1];
+        let arg2 = &args[2];
+
+        println!("{}", arg1);
+        println!("{}", arg2);
+    }
+}
