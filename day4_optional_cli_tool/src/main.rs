d1380910c8bdd4c45d74bf290341154f40a9511a
HarshJ2000 <harsh976324@gmail.com>
2025-11-02 09:12:12 +0530

    added comments

--- day4_optional_cli_tool/src/main.rs
+++ day4_optional_cli_tool/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         std::process::exit(1);
     }
 
+    // checking if the number argument provided is a valid integer or not? using pattern-matching on the args vector, .parse() helps to convert the number argument from string to integer of specified type annotation.
     let num: i32 = match args[1].parse() {
         Ok(num) => num,
         Err(_) => {
@@ -17,6 +18,7 @@ fn main() {
         }
     };
 
+    // checking if the operation flag is provided or not, if yes then which one. Here we used Option<T> for handling the inputs and we have used the .to_string() for the operation flags because the .contains will give us the &String of the args, so to match the types we have to make them referenced.
     let result = if args.contains(&"--double".to_string()) {
         Some(num * 2)
     } else if args.contains(&"--square".to_string()) {
@@ -25,6 +27,7 @@ fn main() {
         None
     };
 
+    // pattern-matching the optionals for operation flags
     match result {
         Some(res) => println!("Result: {}", res),
         None => println!("No operation flag was provided!!!!!!"),
