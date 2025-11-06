d6ddbc1263baf9dbf34d08715ff7676995603df0
HarshJ2000 <harsh976324@gmail.com>
2025-11-06 20:51:28 +0530

    added comments for future references

--- day4_optional_cli_tool/tests/cli.test.js
+++ day4_optional_cli_tool/tests/cli.test.js
@@ -4,15 +4,16 @@ import { Connection } from "@solana/web3.js";
 
 test("CLI doubles number correctly or not?", () => {
   const output = execSync(
+    // execSync is used to execute the mentioned file with some arguments
     "cargo run --manifest-path ../Cargo.toml -- 5 --double"
   )
     .toString()
     .trim();
-  expect(output).toContain("Result: 10");
+  expect(output).toContain("Result: 10"); // .toContain checks if the mentioned string is available in the expected output
 });
 
 test("Solana connection check", async () => {
   const connection = new Connection("http://127.0.0.1:8899");
-  const version = await connection.getVersion();
-  expect(version).toBeTruthy();
+  const version = await connection.getVersion(); // getting version to confirm some connection is available
+  expect(version).toBeTruthy(); // .toBeTruthy checks if the value is converted to boolean will be true
 });
