01e9d4823b946031d5ef28ab244c06e710ab7363
HarshJ2000 <harsh976324@gmail.com>
2025-11-07 20:14:58 +0530

    created basic counter_program

--- day5_counter_program_with_reset_logic/src/lib.rs
+++ day5_counter_program_with_reset_logic/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+entrypoint!(counter_program);
+pub fn counter_program(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instruction_type {
+        InstructionType::Increment(value) => {
+            msg!("Executing Increment.....");
+            counter_data.count += value;
+        }
+        InstructionType::Decrement(value) => {
+            msg!("Executing Increment.....");
+            counter_data.count -= value;
+        }
     }
+
+    counter_data.serialize(&mut *acc.data.borrow_mut())?;
+    println!("Counter updated to: {}", counter_data.count);
+
+    Ok(())
 }
