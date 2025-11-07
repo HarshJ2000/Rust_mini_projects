6c87e8f88f706ef516fcd2ff7fe700f55c80acae
HarshJ2000 <harsh976324@gmail.com>
2025-11-07 17:26:25 +0530

    added comments

--- day5_config_loader/src/main.rs
+++ day5_config_loader/src/main.rs
@@ -8,25 +8,27 @@ fn main() {
 
     let mut input = String::new();
 
-    io::stdin()
+    io::stdin() // Accepting input from user after code compilation
         .read_line(&mut input)
-        .expect("Failed to get inputs!!!!!!");
+        .expect("Failed to get inputs!!!!!!"); // error handling
 
-    let user_input = input.trim();
+    let user_input = input.trim(); // removing new line tag
 
     println!("{:?}", parse_key_value_from_string(user_input));
 }
 
 fn parse_key_value_from_string<'a>(input: &'a str) -> HashMap<&'a str, &'a str> {
+    // lifetimes using 'a
     let mut map = HashMap::new();
 
     for pair_str in input.split(';') {
-        let parts: Vec<&str> = pair_str.split("=").collect();
+        // splitting the string literal onto ';'
+        let parts: Vec<&str> = pair_str.split("=").collect(); // iterating over parts around '=' and collecting them
 
         if parts.len() == 2 {
             let key = parts[0].trim();
             let value = parts[1].trim();
-            map.insert(key, value);
+            map.insert(key, value); // .insert method applied on hashmap
         }
     }
     map
