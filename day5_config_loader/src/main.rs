cecd1a92c87ad0b03c828ca30f789b84453d13fd
HarshJ2000 <harsh976324@gmail.com>
2025-11-07 17:16:02 +0530

    implemented lifetimes and borrowing

--- day5_config_loader/src/main.rs
+++ day5_config_loader/src/main.rs
@@ -1,4 +1,4 @@
-// Lifetimes and borrowing 
+// Lifetimes and borrowing
 // Program to take a string as an input and convert it into key/value pair which is of struct type
 
 use std::{collections::HashMap, io};
@@ -17,15 +17,15 @@ fn main() {
     println!("{:?}", parse_key_value_from_string(user_input));
 }
 
-fn parse_key_value_from_string(input: &str) -> HashMap<String, String> {
+fn parse_key_value_from_string<'a>(input: &'a str) -> HashMap<&'a str, &'a str> {
     let mut map = HashMap::new();
 
     for pair_str in input.split(';') {
         let parts: Vec<&str> = pair_str.split("=").collect();
 
         if parts.len() == 2 {
-            let key = parts[0].trim().to_string();
-            let value = parts[1].trim().to_string();
+            let key = parts[0].trim();
+            let value = parts[1].trim();
             map.insert(key, value);
         }
     }
