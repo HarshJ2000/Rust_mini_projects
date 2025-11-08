b7b563ec6ed68ac23239522ed15f5c4242c79bbe
HarshJ2000 <harsh976324@gmail.com>
2025-11-08 20:36:44 +0530

    Math operations using generics

--- day6_mathops_using_generics/src/main.rs
+++ day6_mathops_using_generics/src/main.rs
@@ -1,5 +1,46 @@
-// Math operations using generics
+// Math Operations using Generics
+use std::ops::{Add, Div, Mul, Sub};
+
+pub struct MathOps<T> {
+    pub value: T,
+}
+
+impl<T> MathOps<T>
+where
+    T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
+{
+    pub fn new(v: T) -> Self {
+        Self { value: v }
+    }
+
+    pub fn add(&mut self, rhs: T) {
+        self.value = self.value + rhs;
+    }
+
+    pub fn sub(&mut self, rhs: T) {
+        self.value = self.value - rhs;
+    }
+
+    pub fn mul(&mut self, rhs: T) {
+        self.value = self.value * rhs;
+    }
+
+    pub fn div(&mut self, rhs: T) {
+        self.value = self.value / rhs;
+    }
+
+    pub fn get(&self) -> T {
+        self.value
+    }
+}
 
 fn main() {
-        
+    let mut num = MathOps::new(10);
+
+    num.add(100);
+    num.sub(10);
+    num.mul(2);
+    num.div(100);
+
+    println!("Result: {}", num.get());
 }
