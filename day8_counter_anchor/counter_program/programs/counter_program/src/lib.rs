e1c4ccdf90abd60b88148d176e34bf3c00e82c04
HarshJ2000 <harsh976324@gmail.com>
2025-11-20 23:40:42 +0530

    data account creation logic

--- day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
+++ day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 
 declare_id!("AzrUyx3XLhFWipfNMfPQgjjQMZcsiLcJ6KY2VA5Mpbnj");
 
+// Program Logic
 #[program]
 pub mod counter_program{
     use super::*;
@@ -16,3 +17,16 @@ pub mod counter_program{
 }
 
 
+// Data account creation
+pub struct Initialize<'info>{
+    #[account(
+        init, payer = signer, space = 8+8+32,
+        seeds = [b"counter", authority.key().as_ref()],
+        bump
+    )]
+    pub new_account:Account<'info, NewAccount>
+
+    #[account(mut)]
+    pub signer: Signer<'info>
+    pub program_id: Program<'info, System>
+}
