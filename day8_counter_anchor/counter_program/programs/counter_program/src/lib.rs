dd48d227c506e8a87cacc65b4741601646aa2142
HarshJ2000 <harsh976324@gmail.com>
2025-11-21 12:25:08 +0530

    fixed initialize struct

--- day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
+++ day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
@@ -2,7 +2,6 @@ use anchor_lang::prelude::*;
 
 declare_id!("AzrUyx3XLhFWipfNMfPQgjjQMZcsiLcJ6KY2VA5Mpbnj");
 
-// Program Logic
 #[program]
 pub mod counter_program{
     use super::*;
@@ -16,17 +15,17 @@ pub mod counter_program{
     pub fn reset(ctx:Context<Reset>) -> Result(<>){}
 }
 
-
 // Data account creation
+#[derive(Accounts)]
 pub struct Initialize<'info>{
     #[account(
         init, payer = signer, space = 8+8+32,
         seeds = [b"counter", authority.key().as_ref()],
         bump
     )]
-    pub new_account:Account<'info, NewAccount>
+    pub counter_acc : Account<'info, Counter>,
 
     #[account(mut)]
-    pub signer: Signer<'info>
-    pub program_id: Program<'info, System>
+    pub authority : Signer<'info>,
+    pub system_program : Program<'info, System>
 }
