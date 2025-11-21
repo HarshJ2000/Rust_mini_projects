f9909767203903dafc6001ee172ef35ff2e0502d
HarshJ2000 <harsh976324@gmail.com>
2025-11-21 17:45:20 +0530

    Fixed typoerrors

--- day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
+++ day8_counter_anchor/counter_program/programs/counter_program/src/lib.rs
@@ -2,61 +2,62 @@ use anchor_lang::prelude::*;
 
 declare_id!("AzrUyx3XLhFWipfNMfPQgjjQMZcsiLcJ6KY2VA5Mpbnj");
 
+// Program Logic
 #[program]
-pub mod counter_program{
+pub mod counter_program {
     use super::*;
 
-    pub fn initialize(ctx:Context<Initialize>) -> Result(<>){}
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {}
 
-    pub fn increment(ctx:Context<Mutate>) -> Result(<>){}
+    pub fn increment(ctx: Context<Mutate>) -> Result<()> {}
 
-    pub fn decrement(ctx:Context<Mutate>) -> Result(<>){}
+    pub fn decrement(ctx: Context<Mutate>) -> Result<()> {}
 
-    pub fn reset(ctx:Context<Reset>) -> Result(<>){}
+    pub fn reset(ctx: Context<Reset>) -> Result<()> {}
 }
 
 // Data account creation
 #[derive(Accounts)]
-pub struct Initialize<'info>{
+pub struct Initialize<'info> {
     #[account(
         init, payer = signer, space = 8+8+32,
         seeds = [b"counter", authority.key().as_ref()],
         bump,
     )]
-    pub counter_acc : Account<'info, Counter>,
+    pub counter_acc: Account<'info, Counter>,
 
     #[account(mut)]
-    pub authority : Signer<'info>,
-    pub system_program : Program<'info, System>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 // Mutate into Data Accounts
 #[derive(Accounts)]
-pub struct Mutate<'info>{
+pub struct Mutate<'info> {
     #[account(
         mut,
         seeds = [b"counter", counter.authority.as_ref()],
         bump,
     )]
-    pub counter_acc : Account<'info, Counter>,
-    pub authority : Signer<'info>,
+    pub counter_acc: Account<'info, Counter>,
+    pub authority: Signer<'info>,
 }
 
 // Resetting Counter Account Struct
 #[derive(Accounts)]
-pub struct Reset<'info>{
+pub struct Reset<'info> {
     #[account(
         mut, has_one = authority,
         seeds = [b"counter", counter.authority.as_ref()],
         bump,
     )]
-    pub counter_acc : Account<'info, Counter>,
-    pub authority : Signer<'info>,
+    pub counter_acc: Account<'info, Counter>,
+    pub authority: Signer<'info>,
 }
 
 // Account Data Account
 #[account]
-pub struct Counter{
+pub struct Counter {
     pub count: u64,
     pub authority: Pubkey,
 }
