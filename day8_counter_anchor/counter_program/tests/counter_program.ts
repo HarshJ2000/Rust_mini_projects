01057900b65d6ea105e54abf69c1e39f76f5f4f2
HarshJ2000 <harsh976324@gmail.com>
2025-11-22 14:01:26 +0530

    tests for the counter program

--- day8_counter_anchor/counter_program/tests/counter_program.ts
+++ day8_counter_anchor/counter_program/tests/counter_program.ts
@@ -1,16 +1,89 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CounterProgram } from "../target/types/counter_program";
+import { expect } from "chai";
 
 describe("counter_program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.counterProgram as Program<CounterProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Is Initialized!!!", async () => {
+    const authority = provider.wallet.publicKey;
+
+    const [counterPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter"), authority.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+      .initialize(new anchor.BN(0))
+      .accountsStrict({
+        counterAcc: counterPda,
+        authority,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log("Counter PDA: ", counterPda.toString());
+  });
+
+  it("Increment Counter....", async () => {
+    const authority = provider.wallet.publicKey;
+
+    const [counterPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter"), authority.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+      .increment(new anchor.BN(5))
+      .accounts({
+        counterAcc: counterPda,
+        authority,
+      })
+      .rpc();
+
+    console.log("Counter Incremented by 5...");
+  });
+
+  it("Decrement Counter...", async () => {
+    const authority = provider.wallet.publicKey;
+
+    const [counterPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter"), authority.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+      .decrement(new anchor.BN(2))
+      .accounts({
+        counterAcc: counterPda,
+        authority,
+      })
+      .rpc();
+
+    console.log("Counter Decremented by 2...");
+  });
+
+  it("Reset Counter...", async () => {
+    const authority = provider.wallet.publicKey;
+
+    const [counterPda] = await anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter"), authority.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+      .reset()
+      .accounts({
+        counterAcc: counterPda,
+        authority,
+      })
+      .rpc();
+
+    console.log("Counter reset to 0");
   });
 });
