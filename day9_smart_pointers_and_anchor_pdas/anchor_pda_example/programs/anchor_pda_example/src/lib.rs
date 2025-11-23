140f5fd496738fd4a0759727d467447bad9d73b7
HarshJ2000 <harsh976324@gmail.com>
2025-11-24 00:53:52 +0530

    finding pda using anchor program

--- day9_smart_pointers_and_anchor_pdas/anchor_pda_example/programs/anchor_pda_example/src/lib.rs
+++ day9_smart_pointers_and_anchor_pdas/anchor_pda_example/programs/anchor_pda_example/src/lib.rs
@@ -7,10 +7,29 @@ pub mod anchor_pda_example {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("PDA Account: {}", ctx.accounts.user_pda.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 8,
+        seeds = [b"user", user.key().as_ref()],
+        bump,
+    )]
+    pub user_pda: Account<'info, UserAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UserAccount {
+    pub authority: Pubkey,
+    pub data: u64,
+}
