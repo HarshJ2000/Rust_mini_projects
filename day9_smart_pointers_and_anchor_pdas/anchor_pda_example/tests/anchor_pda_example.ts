b534bf2c35cf0c909d568c9ce1b380b8537f0e20
HarshJ2000 <harsh976324@gmail.com>
2025-11-24 01:57:38 +0530

    tests for checking if proper execution of PDA is done or not by anchor program

--- day9_smart_pointers_and_anchor_pdas/anchor_pda_example/tests/anchor_pda_example.ts
+++ day9_smart_pointers_and_anchor_pdas/anchor_pda_example/tests/anchor_pda_example.ts
@@ -4,13 +4,30 @@ import { AnchorPdaExample } from "../target/types/anchor_pda_example";
 
 describe("anchor_pda_example", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.anchorPdaExample as Program<AnchorPdaExample>;
+  const program = anchor.workspace
+    .anchorPdaExample as Program<AnchorPdaExample>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+
+    const [pda] = await anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("user"), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .initialize()
+      .accountsStrict({
+        userPda: pda,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
     console.log("Your transaction signature", tx);
+    console.log("Expected PDA: ", pda.toBase58());
   });
 });
