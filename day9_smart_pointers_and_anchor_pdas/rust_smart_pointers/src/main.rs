1dcb7ce5dce0a9bd43988ba78d0158867f54409c
HarshJ2000 <harsh976324@gmail.com>
2025-11-23 23:57:53 +0530

    Shared ownership for references using Rc smart pointer

--- day9_smart_pointers_and_anchor_pdas/rust_smart_pointers/src/main.rs
+++ day9_smart_pointers_and_anchor_pdas/rust_smart_pointers/src/main.rs
@@ -1,5 +1,7 @@
 mod box_example;
+mod rc_example;
 
 fn main() {
     box_example::run();
+    rc_example::run_rc_example();
 }
---
+++ day9_smart_pointers_and_anchor_pdas/rust_smart_pointers/src/rc_example.rs
@@ -0,0 +1,17 @@
+// Shared Ownership for References
+use std::rc::Rc;
+
+pub fn run_rc_example() {
+    let a = Rc::new(10);
+    println!("Refernce count initially: {}", Rc::strong_count(&a));
+
+    let _b = Rc::clone(&a);
+    println!("After cloning 1 time: {}", Rc::strong_count(&a));
+
+    {
+        let _c = Rc::clone(&a);
+        println!("After cloning 2nd time: {}", Rc::strong_count(&a));
+    } // reference count will decrease as _c will get out of scope
+
+    println!("Reference count finally: {}", Rc::strong_count(&a));
+}
