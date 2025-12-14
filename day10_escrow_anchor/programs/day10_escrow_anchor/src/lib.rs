5976259f820ef5d0fb3682a45a7bd48b27d0abf6
HarshJ2000 <harsh976324@gmail.com>
2025-12-14 16:51:22 +0530

    added comments

--- day10_escrow_anchor/programs/day10_escrow_anchor/src/lib.rs
+++ day10_escrow_anchor/programs/day10_escrow_anchor/src/lib.rs
@@ -97,16 +97,21 @@ pub mod day10_escrow_anchor {
 
         let amount = escrow.initializer_amount;
 
+        // seperately defined initializer_key because we cannot call .key() inside the &[]
         let initializer_key = escrow.initializer.key();
+        // Seeds reqruired for the signing of the withdraw transaction
         let seeds = &[b"vault", initializer_key.as_ref(), &[escrow.bump]];
+        // signer_seeds is the way in which the transfer method of CPI wants the seeds to be provided
         let signer_seeds = &[&seeds[..]];
 
+        // Building the cpi accounts required for transfer
         let cpi_accounts = anchor_spl::token::Transfer {
             from: ctx.accounts.vault_ata.to_account_info(),
             to: ctx.accounts.initializer_ata.to_account_info(),
             authority: ctx.accounts.vault_authority.to_account_info(),
         };
 
+        // executing the cpi transfer
         anchor_spl::token::transfer(
             CpiContext::new_with_signer(
                 ctx.accounts.token_program.to_account_info(),
@@ -116,6 +121,7 @@ pub mod day10_escrow_anchor {
             amount,
         )?;
 
+        // updating the state for the escrow
         escrow.state = EscrowStatus::Cancelled;
 
         Ok(())
